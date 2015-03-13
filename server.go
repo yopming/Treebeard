0abd4b3e3310a05139f4df2c859e2dcf1c6d53cb
yopming <yopming@gmail.com>
2015-03-13 16:10:52 +0800

    add example of url;

--- server.go
+++ server.go
@@ -1,9 +1,15 @@
 package main
 
+//http://demo.vemic.com:5000/files/AAPP/旅行箱/1.0/test
+
 import (
     "log"
     "net/http"
+    "os"
+    "sort"
+    "io/ioutil"
 )
+
 
 func main() {
     fs := http.FileServer(http.Dir("static"))
@@ -12,3 +18,10 @@ func main() {
     log.Println("Listening...")
     http.ListenAndServe(":3000", nil)
 }
+
+func FileMTime(file string) (int64, error) {
+    f, err := os.Stat(file)
+    if err != nil {
+        return 0, err
+    }
+    retunr f.ModT
