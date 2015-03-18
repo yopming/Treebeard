1ec0588c8903a7d8662c74e833fbb873a02445c7
yopming <yopming@gmail.com>
2015-03-18 23:25:32 +0800

    add library;

--- server.go
+++ server.go
@@ -9,6 +9,7 @@ import (
     "path/filepath"
     //"encoding/json"
     "github.com/codegangsta/martini"
+    "github.com/martini-contrib/render"
 )
 
 type Response struct {
@@ -23,19 +24,27 @@ func main() {
     path_prefix := "Z:\\"
 
     m := martini.Classic()
+    m.Use(render.Renderer())
 
     // Parse the URL
-    m.Get("/files/**", func(w http.ResponseWriter, r *http.Request) {
+    m.Get("/files/**", func(
+        w http.ResponseWriter,
+        r *http.Request,
+        render render.Render,
+        params martini.Params
+    ) {
+
         message := r.URL.RequestURI()
         prefix := "/files/"
         elite := strings.Split(message, prefix)[1]
-        //collection := make(map[string]Json)
 
         if len(elite) > 0 {
             // Step in the real folder
             real_path := filepath.ToSlash(path_prefix) + elite
 
-            itemInfo := map[string]map[string]string{}
+            //itemInfo := map[string]map[string]string{}
+            itemInfo := make(map[string]interface{})
+            item     := make(map[string]interface{})
 
             filepath.Walk(real_path, func(path string, fileinfo os.FileInfo, err error) error {
                 f, err := os.Stat(path)
@@ -43,18 +52,28 @@ func main() {
                     panic(err)
                 }
 
-                itemInfo["name"]["name"] = f.Name()
-                itemInfo["name"]["time"] = f.ModTime().Format("01-02 15:04:05") // time.time to string
-                itemInfo["name"]["type"] = strconv.FormatInt(f.Size(), 10) // int64 to string
+                if f.IsDir() {
+                    item["type"] = "dir"
+                    item["path"] = real_path
+                } else {
+                    item["type"] = "file"
+                    item["path"] = real_path + "/" + f.Name()
+                }
+                item["time"] = f.ModTime().Format("2004-01-02 15:04:22") // time to string
+                item["type"] = strconv.FormatInt(f.Size(), 10) // int64 to string
 
-                //for key, value := range itemInfo {
-                    //fmt.Printf("%s: %s \n", key, value)
-                //}
+                itemInfo["name"] = item
 
                 return nil
             })
 
+            collection, err_collection := json.Marshal(itemInfo)
+            if err_collection != nil {
+                fmt.Println(err_collection)
+            }
         }
+
+        render.JSON(200, collection)
     });
 
     m.Run()
