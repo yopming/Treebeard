85768ee077a36e46762a7858a1d39a3dcd610404
yopming <yopming@gmail.com>
2015-04-16 17:23:25 +0800

    add routing for '/file' url and build an new exe file;

--- server.go
+++ server.go
@@ -30,50 +30,63 @@ func main() {
 	m.Use(martini.Static(filepath.ToSlash(path_prefix)))
 
 	// Parse the URL
-	m.Get("/files/**", func(w http.ResponseWriter, r *http.Request, render render.Render, params martini.Params) {
+	m.Get("/files", func(w http.ResponseWriter, r *http.Request, render render.Render, params martini.Params) {
+		real_path := path.Join(filepath.ToSlash(path_prefix))
+		var elite string
+		itemInfo := ReadGraphic(real_path, elite)
+		render.JSON(200, itemInfo)
+	})
 
+	m.Get("/files/**", func(w http.ResponseWriter, r *http.Request, render render.Render, params martini.Params) {
 		message := r.URL.RequestURI()
 		prefix := "/files/"
 		splitter := strings.Split(message, prefix)
 		elite := DecodeURI(splitter[1])
 
 		if len(elite) >= 0 {
-			// Step in the real folder
 			real_path := path.Join(filepath.ToSlash(path_prefix), elite)
-
-			itemInfo := make(map[string]interface{})
-
-			files, _ := ioutil.ReadDir(real_path)
-			i := 0
-
-			for _, f := range files {
-				// non-hidden files(starting with .)
-				if !strings.HasPrefix(f.Name(), ".") {
-					item := make(map[string]interface{})
-
-					if f.IsDir() {
-						item["type"] = "Directory"
-						item["down"] = elite + "/" + f.Name()
-					} else {
-						item["type"] = "File"
-						item["down"] = "http://demo.vemic.com/demos/ins/设计稿/" + elite + "/" + f.Name()
-					}
-					if !strings.HasPrefix(f.Name(), ".") {
-						item["name"] = f.Name()
-					}
-					item["time"] = f.ModTime().Format(time.RFC3339)
-					item["size"] = strconv.FormatInt(f.Size(), 10)
-					itemInfo[strconv.Itoa(i)] = item
-				}
-				i++
-			}
-
+			ReadGraphic(real_path, elite)
+			itemInfo := ReadGraphic(real_path, elite)
 			render.JSON(200, itemInfo)
 		}
 
 	})
 
 	m.RunOnAddr(":54321")
+}
+
+/**
+ * Read directory and return json
+ */
+func ReadGraphic(real_path string, elite string) interface{} {
+	// Step in the real folder
+	itemInfo := make(map[string]interface{})
+	files, _ := ioutil.ReadDir(real_path)
+	i := 0
+
+	for _, f := range files {
+		// non-hidden files(starting with .)
+		if !strings.HasPrefix(f.Name(), ".") {
+			item := make(map[string]interface{})
+
+			if f.IsDir() {
+				item["type"] = "Directory"
+				item["down"] = elite + "/" + f.Name()
+			} else {
+				item["type"] = "File"
+				item["down"] = "http://demo.vemic.com/demos/ins/设计稿/" + elite + "/" + f.Name()
+			}
+			if !strings.HasPrefix(f.Name(), ".") {
+				item["name"] = f.Name()
+			}
+			item["time"] = f.ModTime().Format(time.RFC3339)
+			item["size"] = strconv.FormatInt(f.Size(), 10)
+			itemInfo[strconv.Itoa(i)] = item
+		}
+		i++
+	}
+
+	return itemInfo
 }
 
 /**
