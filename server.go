ea26227f3102832b0c73be9b3078f94f76137265
yopming <yopming@gmail.com>
2015-04-16 15:03:28 +0800

    remove example files and update the return of function;

--- server.go
+++ server.go
@@ -1,67 +1,98 @@
 package main
 
 import (
-    //"fmt"
-    "time"
-    "strings"
-    "strconv"
-    "net/http"
-    "io/ioutil"
-    "path/filepath"
-    //"encoding/json"
-    "github.com/go-martini/martini"
-    "github.com/martini-contrib/render"
+	"github.com/go-martini/martini"
+	"github.com/martini-contrib/render"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"path"
+	"path/filepath"
+	"strconv"
+	"strings"
+	"time"
 )
 
-
 func main() {
-    // Static File Folder
-    path_prefix := "Z:\\"
+	// Static File Folder
+	path_prefix := "D:\\webroot\\e\\website\\demos\\ins\\设计稿"
 
-    m := martini.Classic()
-    m.Use(render.Renderer())
-    m.Use(martini.Static(filepath.ToSlash(path_prefix)))
+	m := martini.Classic()
+	m.Use(render.Renderer())
+	m.Use(martini.Static(filepath.ToSlash(path_prefix)))
 
-    // Parse the URL
-    m.Get("/files/**", func(w http.ResponseWriter, r *http.Request, render render.Render, params martini.Params) {
+	// Parse the URL
+	m.Get("/files/**", func(w http.ResponseWriter, r *http.Request, render render.Render, params martini.Params) {
 
-        message := r.URL.RequestURI()
-        prefix := "/files/"
-        elite := strings.Split(message, prefix)[1]
+		message := r.URL.RequestURI()
+		prefix := "/files/"
+		splitter := strings.Split(message, prefix)
+		elite := DecodeURI(splitter[1])
 
-        if len(elite) > 0 {
-            // Step in the real folder
-            real_path := filepath.ToSlash(path_prefix) + elite
+		if len(elite) >= 0 {
+			// Step in the real folder
+			real_path := path.Join(filepath.ToSlash(path_prefix), elite)
 
-            itemInfo := make(map[string]interface{})
+			itemInfo := make(map[string]interface{})
 
-            files, _ := ioutil.ReadDir(real_path)
-            i := 0
+			files, _ := ioutil.ReadDir(real_path)
+			i := 0
 
-            for _, f := range files {
-                // non-hidden files(starting with .)
-                if !strings.HasPrefix(f.Name(), ".") {
-                    item := make(map[string]interface{})
+			for _, f := range files {
+				// non-hidden files(starting with .)
+				if !strings.HasPrefix(f.Name(), ".") {
+					item := make(map[string]interface{})
 
-                    if f.IsDir() {
-                        item["type"] = "Directory"
-                    } else {
-                        item["type"] = "File"
-                    }
-                    if !strings.HasPrefix(f.Name(), ".") {
-                        item["name"] = f.Name()
-                    }
-                    item["time"] = f.ModTime().Format(time.RFC3339)
-                    item["size"] = strconv.FormatInt(f.Size(), 10)
-                    itemInfo[strconv.Itoa(i)] = item
-                }
-                i++
-            }
+					if f.IsDir() {
+						item["type"] = "Directory"
+					} else {
+						item["type"] = "File"
+						item["down"] = "http://demo.vemic.com/demos/ins/设计稿/" + elite + "/" + f.Name()
+					}
+					if !strings.HasPrefix(f.Name(), ".") {
+						item["name"] = f.Name()
+					}
+					item["time"] = f.ModTime().Format(time.RFC3339)
+					item["size"] = strconv.FormatInt(f.Size(), 10)
+					itemInfo[strconv.Itoa(i)] = item
+				}
+				i++
+			}
 
-            render.JSON(200, itemInfo)
-        }
+			render.JSON(200, itemInfo)
+		}
 
-    });
+	})
 
-    m.Run()
+	m.RunOnAddr(":54321")
+}
+
+/**
+ * Input: filepath
+ * Output: encode string
+ */
+func EncodeURI(filepath string) string {
+	if len(filepath) < 1 {
+		return ""
+	}
+
+	u := url.URL{}
+	u.Path = filepath
+	return u.String()
+}
+
+/**
+ * Input: URI string
+ * Output: filepath
+ */
+func DecodeURI(uri string) string {
+	if len(uri) < 1 {
+		return ""
+	}
+
+	u, err := url.Parse(uri)
+	if err != nil {
+		return ""
+	}
+	return u.Path
 }
